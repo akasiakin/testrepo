c2313e43cd2ab8382890e229898188321f82df18
akasiakin <akasiakin@elinext.com>
2019-12-09 13:20:55 +0300

    develop: the second

--- file-2.ts
+++ file-2.ts
@@ -1,2 +1,3 @@
 console.log('This is the second file, created in a master branch.');
 console.log('Create develop branch and do the first commit.');
+console.log('Do the second commit into develop branch.');
